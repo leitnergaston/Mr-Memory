0bc84730cf537cd95eeb53d015635e4b0bb9636e
unknown <leitnergaston@gmail.com>
2023-06-19 04:06:19 -0300

    actualizacion 19/6/23 4:06

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 let intentos;
 let puntos;
+let tiempoLimite = 60;
+let tiempoTotal = 0;
+let temporizadorActivo;
 let cantidadTarjetas;
 let iconos = [];
 let selecciones = [];
+let nivelActual = 0;
 let niveles = [
   { nivel: 1, cantidadCartas: 6 },
   { nivel: 2, cantidadCartas: 10 },
   { nivel: 3, cantidadCartas: 14 },
   { nivel: 4, cantidadCartas: 18 },
   { nivel: 5, cantidadCartas: 22 },
-  { nivel: 6, cantidadCartas: 26 },
-  { nivel: 7, cantidadCartas: 30 },
   // Agrega más niveles según sea necesario
 ];
-let nivelActual = 0;
 
-nuevoJuego();
+
+
+// iniciarJuego();
 
 function cargarIconos() {
   iconos = [
@@ -32,7 +35,7 @@ function cargarIconos() {
     '<i><img src="img/11.png" alt=""></i>',
     '<i><img src="img/12.png" alt=""></i>',
     '<i><img src="img/special.png" alt="" class="efecto-rainbow"></i>',
-    
+
   ];
 }
 
@@ -46,7 +49,7 @@ function generarTablero() {
   for (let i = 0; i < cantidadTarjetas; i++) {
     tarjetas.push(`
       <div class="area-tarjeta" onclick="seleccionarTarjeta(${i})">
-          <div class="tarjeta" id="tarjeta${i}">
+      <div class="tarjeta nivel-${nivelActual + 1}" id="tarjeta${i}">
               <div class="cara trasera" id="trasera${i}">
                   ${iconos[i % (cantidadTarjetas / 2)]}
               </div>
@@ -77,7 +80,7 @@ function seleccionarTarjeta(i) {
     tarjeta1.classList.add("efecto-rainbow");
     tarjeta2.classList.add("efecto-rainbow");
   }
-  
+
 }
 
 function deseleccionar(selecciones) {
@@ -108,17 +111,31 @@ function deseleccionar(selecciones) {
     }
 
     if (verificarFin()) {
-      setTimeout(() => {
-        alert(
-          `¡Felicidades! Has encontrado todas las parejas en ${intentos} intentos. Tu puntuación es ${puntos}.`
-        );
-        if (nivelActual < niveles.length - 1) {
+      // Verifica si se han completado todos los niveles
+      if (nivelActual === niveles.length - 1) {
+        setTimeout(juegoFinalizado, 1000);
+      } else {
+        setTimeout(() => {
+          alert(
+            `¡Felicidades! Has encontrado todas las parejas en ${intentos} intentos. Tu puntuación es ${puntos}. Pasa al siguiente nivel.`
+          );
           nivelActual++;
-          document.getElementById("nivel").innerHTML =
-            "Nivel actual: " + niveles[nivelActual].nivel;
+          document.getElementById("nivel").innerHTML = "Nivel actual: " + niveles[nivelActual].nivel;
           generarTablero();
-        }
-      }, 500);
+
+          clearInterval(temporizador);
+          if (nivelActual === 1){ //nivel 2
+            setTimeout(iniciarTemporizador, 2500);
+          } else if (nivelActual === 2){ //nivel 3
+            setTimeout(iniciarTemporizador, 3000);
+          } else if (nivelActual === 3){ //nivel 4
+            setTimeout(iniciarTemporizador, 3500);
+          } else if (nivelActual === 4){ // nivel 5
+            setTimeout(iniciarTemporizador, 3500);
+          }
+
+        }, 500);
+      }
     }
   }, 1000);
 }
@@ -126,21 +143,135 @@ function deseleccionar(selecciones) {
 function verificarFin() {
   for (let i = 0; i < cantidadTarjetas; i++) {
     let trasera = document.getElementById("trasera" + i);
-    if (trasera.style.background != "plum") {
+    if (trasera.style.background !== "plum") {
       return false;
     }
   }
   return true;
 }
 
-function nuevoJuego() {
+
+
+
+
+
+
+let temporizador;
+
+function iniciarJuego() {
+  clearInterval(temporizador); // Detener el temporizador actual (si existe)
+
+  
+
+  // Restablecer valores iniciales
   puntos = 0;
   intentos = 0;
+  nivelActual = 0;
+  tiempoLimite = 60;
+  tiempoTotal = 0;
+
+  // Actualizar elementos HTML
   document.getElementById("puntos").innerHTML = "Puntos: " + puntos;
   document.getElementById("intentos").innerHTML = "Intentos: " + intentos;
-  nivelActual = 0;
-  document.getElementById("nivel").innerHTML =
-    "Nivel actual: " + niveles[nivelActual].nivel;
+  document.getElementById("nivel").innerHTML = "Nivel actual: " + niveles[nivelActual].nivel;
+  document.getElementById("tiempoRestante").innerHTML = "Tiempo restante: " + tiempoLimite + "s";
+
+
   cargarIconos();
   generarTablero();
+
+  setTimeout(() => {
+    iniciarTemporizador();
+  }, 1500);
+
+
+  
+
 }
+
+
+
+function iniciarTemporizador() {
+
+  temporizador = setInterval(() => {
+    tiempoLimite--;
+    tiempoTotal++;
+    mostrarTiempoRestante();
+
+    if (tiempoLimite === 0) {
+      clearInterval(temporizador);
+      tiempoAgotado();
+    }
+  }, 1000);
+}
+
+function mostrarTiempoRestante() {
+  document.getElementById("tiempoRestante").innerHTML = "Tiempo restante: " + tiempoLimite + "s";
+}
+
+function tiempoAgotado() {
+  //detener el temporizador
+  clearInterval(temporizador);
+  alert("¡Tiempo agotado! Juego terminado.");
+}
+
+function juegoFinalizado() {
+  const nombreJugador = document.getElementById("nombreJugador").value;
+  const registro = {
+    nombre: nombreJugador,
+    puntuacion: puntos,
+    intentos: intentos,
+    tiempo: tiempoTotal
+  };
+
+  // Obtener la tabla de clasificación actual desde el almacenamiento local
+  let clasificacion = JSON.parse(localStorage.getItem("clasificacion")) || [];
+
+  // Agregar el nuevo registro a la tabla de clasificación
+  clasificacion.push(registro);
+
+  // Ordenar la tabla de clasificación por puntuación de mayor a menor
+  clasificacion.sort((a, b) => b.puntuacion - a.puntuacion);
+
+  // Guardar la tabla de clasificación actualizada en el almacenamiento local
+  localStorage.setItem("clasificacion", JSON.stringify(clasificacion));
+
+
+  clearInterval(temporizador);
+  alert(
+    `¡Felicidades! Has completado todos los niveles.\n\nPuntos totales: ${puntos}\nIntentos totales: ${intentos}\nTiempo total: ${tiempoTotal} segundos`
+  );
+  mostrarClasificacion();
+
+}
+
+
+function mostrarClasificacion() {
+  document.getElementById("tablero").style.display = 'none';
+
+  const clasificacion = JSON.parse(localStorage.getItem("clasificacion"));
+
+  if (clasificacion) {
+    let tablaHTML = "<h2>Tabla de Clasificación</h2>";
+    tablaHTML += "<table>";
+    tablaHTML += "<tr><th>Nombre</th><th>Puntuación</th><th>Intentos</th><th>Tiempo</th></tr>";
+
+    for (let i = 0; i < clasificacion.length; i++) {
+      tablaHTML += `<tr><td>${clasificacion[i].nombre}</td><td>${clasificacion[i].puntuacion}</td><td>${clasificacion[i].intentos}</td><td>${clasificacion[i].tiempo} seg</td></tr>`;
+    }
+
+    tablaHTML += "</table>";
+
+    document.getElementById("tablaClasificacion").innerHTML = tablaHTML;
+  }
+}
+
+
+
+
+
+
+
+
+
+
